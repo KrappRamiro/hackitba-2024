7c28d28fc9a1c4c1f6039592781e269ee7a493bb
ariel.serato <ariel.serato@accenture.com>
2024-04-06 14:40:25 -0300

    create new modal and connect with backend

--- Jano-frontend/src/components/Sleep/SleepForm.tsx
+++ Jano-frontend/src/components/Sleep/SleepForm.tsx
@@ -1,44 +1,55 @@
 // create a form that captures the sleep time of the user and the interruption level and post it on the backend
 //
 
+import { FormControl, FormControlLabel, FormLabel, Radio, RadioGroup, TextField } from "@mui/material";
+import axios from "axios";
+import React from "react";
 import { useState } from "react";
 import { JSX } from "react/jsx-runtime";
 
 export default function SleepForm() {
-  const [sleepTime, setSleepTime] = useState("");
-  const [interruptionLevel, setInterruptionLevel] = useState("");
+	const [sleepTime, setSleepTime] = useState(0);
+	const [interruptionLevel, setInterruptionLevel] = useState("");
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    //const response = await axios.post("/api/sleep", { sleepTime, interruptionLevel });
-    console.log(sleepTime, interruptionLevel);
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInterruptionLevel((event.target as HTMLInputElement).value);
   };
+  const handleChangeSleep = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSleepTime(Number((event.target as HTMLInputElement).value));
+  };
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log( sleepTime)
+    const req = {
+      "userId": "609d91d9e2d6d41768a092d8",
+      "date": "2024-04-06",
+      "dailySleepHours": sleepTime,
+      "interruptionLevel": interruptionLevel,
+      "userFeelsDisorder": false
+  }
+		//const response = await axios.post("/api/sleep", { sleepTime, interruptionLevel });
+		axios.post("http://localhost:3000/sleeps", 
+    req
+    ).then((response) => {
+      console.log(response);
+    }).catch((error) => {
+      console.error("Error:", error);
+    }
+    );
+	};
 
-  return (
-    <form onSubmit={handleSubmit} className="flex flex-col">
-      <label className="mb-2" htmlFor="sleepTime">
-        Sleep time
-      </label>
-      <input
-        id="sleepTime"
-        type="time"
-        value={sleepTime}
-        onChange={(e) => setSleepTime(e.target.value)}
-        className="mb-4"
-      />
-      <label className="mb-2" htmlFor="interruptionLevel">
-        Interruption level
-      </label>
-      <input
-        id="interruptionLevel"
-        type="number"
-        value={interruptionLevel}
-        onChange={(e) => setInterruptionLevel(e.target.value)}
-        className="mb-4"
-      />
-      <button className="bg-green-500 text-white rounded-lg py-2" type="submit">
-        Submit
-      </button>
-    </form>
-  );
+	return (
+		<form onSubmit={handleSubmit} className="flex flex-col">
+			<TextField id="standard-basic" label="Horas de sueño" variant="standard" onChange={handleChangeSleep}/>
+			<FormLabel id="demo-radio-buttons-group-label">Cuantas veces te despertaste?</FormLabel>
+			<RadioGroup onChange={handleChange} aria-labelledby="demo-radio-buttons-group-label" defaultValue="Never" name="radio-buttons-group">
+				<FormControlLabel value="no interruptions" control={<Radio />} label="no interruptions" />
+				<FormControlLabel value="a few times" control={<Radio />} label="a few times" />
+				<FormControlLabel value="too much interruptions" control={<Radio />} label="too much interruptions" />
+			</RadioGroup>
+			<button className="bg-green-500 text-white rounded-lg py-2" type="submit">
+				Enviar
+			</button>
+		</form>
+	);
 }
--- Jano-frontend/src/components/food/ModalUploadFood.tsx
+++ Jano-frontend/src/components/food/ModalUploadFood.tsx
@@ -1,28 +1,29 @@
-import Modal from "../Modal"
+import Modal from "../Sleep/ModalSleep"
 
-export default function ModalUploadFood({ open, setOpen}) {
+export default function ModalUploadFood() {
     return (
-        <Modal open={open} onClose={() => setOpen(false)}>
-          <div className="text-center w-56">
+      <></>
+        // <Modal open={open} onClose={() => setOpen(false)}>
+        //   <div className="text-center w-56">
             
-            <div className="mx-auto my-4 w-48">
-              <h3 className="text-lg font-black text-white mb-5">¿Quedaste satisfecho?</h3>
-              <p className="text-sm text-gray-400 my-5">
-                Indícanos qué tan grande consideras que fué la porción
-              </p>
-            </div>
-            <div className="flex text-sm px-2 justify-center gap-4">
-              <button className="h-9 px-3 bg-yellow-600 rounded-full">
-                Poca
-              </button>
-              <button className="h-9 px-3 bg-lime-600 rounded-full">
-                Suficiente
-              </button>
-              <button className="h-9 px-3 bg-orange-600 rounded-full">
-                Abundante
-              </button>
-            </div>
-          </div>
-        </Modal>
+        //     <div className="mx-auto my-4 w-48">
+        //       <h3 className="text-lg font-black text-white mb-5">¿Quedaste satisfecho?</h3>
+        //       <p className="text-sm text-gray-400 my-5">
+        //         Indícanos qué tan grande consideras que fué la porción
+        //       </p>
+        //     </div>
+        //     <div className="flex text-sm px-2 justify-center gap-4">
+        //       <button className="h-9 px-3 bg-yellow-600 rounded-full">
+        //         Poca
+        //       </button>
+        //       <button className="h-9 px-3 bg-lime-600 rounded-full">
+        //         Suficiente
+        //       </button>
+        //       <button className="h-9 px-3 bg-orange-600 rounded-full">
+        //         Abundante
+        //       </button>
+        //     </div>
+        //   </div>
+        // </Modal>
     )
   }
